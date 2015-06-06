8281eb3d456d402cf647c860fa10115778b5816d
Konrad Förstner <konrad@foerstner.org>
2015-06-06 22:45:38 +0200

    Working extract subcommand

--- covernantlib/extract.py
+++ covernantlib/extract.py
@@ -3,22 +3,28 @@ from datetime import datetime
 import sys
 import math
 import numpy as np
+import pandas as pd
 from covernantlib.wiggle import WiggleParser
 
 
 def extract(args):
     coverage_extractor = CoverageExtractor(
-        args.position_file, args.coverage_file, args.output_prefix,
+        args.coordinate_file, args.coverage_file, args.output_prefix,
         args.flip_reverse_strand, args.ignore_zeros,
-        args.skip_missing_coverages)
+        args.skip_missing_coverages, args.matrix_alignment)
     log("Reading wiggle file")
     coverage_extractor.read_wiggle_file()
-    log("Reading position files")
-    coverage_extractor.read_position_file()
-    log("Calculating averages")
+    log("Reading coordinate files")
+    coverage_extractor.read_coordinate_file()
+    log("Generate coverage matrix")
+    coverage_extractor.extract_coverages()
+    coverage_extractor.generate_coverage_matrix()
+    log("Writing matrix")
+    coverage_extractor.write_matrix_to_file()
+    log("Calculating and writing averages")
     coverage_extractor.calc_averages()
 
-
+    
 def log(msg):
     sys.stderr.write("{} - {}\n".format(
         datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'), msg))
@@ -26,71 +32,135 @@ def log(msg):
 
 class CoverageExtractor(object):
     """ """
-    def __init__(self, position_file, coverage_file, output_prefix,
-                 flip_reverse_strand, ignore_zeros, skip_missing_coverages):
-        self._position_file = position_file
+    def __init__(self, coordinate_file, coverage_file, output_prefix,
+                 flip_reverse_strand, ignore_zeros, skip_missing_coverages,
+                 matrix_alignment):
+        self._coordinate_file = coordinate_file
         self._coverage_file = coverage_file
         self._output_prefix = output_prefix
         self._flip_reverse_strand = flip_reverse_strand
         self._ignore_zeros = ignore_zeros
         self._skip_missing_coverages = skip_missing_coverages
+        self._matrix_alignment = matrix_alignment
         self._replicons_and_coverages = {}
-        self._all_coverage_lists = []
+        self._coordinates = []
+        self._coverage_lists = []
 
     def read_wiggle_file(self):
         wiggle_parser = WiggleParser()
         for entry in wiggle_parser.entries(open(self._coverage_file)):
-            # Return dictionaries of the position and absolute coverage values
+            # Return dictionaries of the coordinate and absolute
+            # coverage values
             self._replicons_and_coverages[entry.replicon] = dict(
                 [(pos, abs(value)) for pos, value in entry.pos_value_pairs])
 
-    def read_position_file(self):
-        for row in csv.reader(open(self._position_file), delimiter="\t"):
+    def read_coordinate_file(self):
+        for row in csv.reader(open(self._coordinate_file), delimiter="\t"):
             if row[0].startswith("#"):
                 continue
-            pos_1, pos_2 = [int(pos) for pos in row[1:3]]
-            assert pos_1 <= pos_2
+            start, end = [int(pos) for pos in row[1:3]]
+            assert start <= end
             strand = row[3]
             assert strand in ["+", "-"]
-            pos_range = round((pos_2 - pos_1) / 2)
-            positions_and_coverage = self._replicons_and_coverages[row[0]]
-            mid_pos = pos_1 + pos_range
-            positions = range(mid_pos - pos_range, mid_pos + pos_range)
-            coverages = [positions_and_coverage.get(
-                pos, 0.0) for pos in positions]
-            if self._flip_reverse_strand and strand == "-":
+            self._coordinates.append(
+                {"replicon": row[0], "start": start,
+                 "end": end, "strand": strand})
+
+    def extract_coverages(self):
+        for coordinate in self._coordinates:
+            self._check_coordinate_validity(coordinate)
+            coverages = [
+                self._replicons_and_coverages[coordinate["replicon"]].get(
+                    pos, 0.0)
+                for pos in range(coordinate["start"], coordinate["end"])
+            ]
+            if self._flip_reverse_strand and coordinate["strand"] == "-":
                 coverages = coverages[::-1]
-            self._all_coverage_lists.append(coverages)
+            self._coverage_lists.append(coverages)
+
+    def _check_coordinate_validity(self, coordinate):
+        pass
+            
+    def generate_coverage_matrix(self):
+        max_range = max(
+            [len(coverages) for coverages in self._coverage_lists])
+        self._coverage_df = self._init_coverage_dataframe(max_range)
+        for coordinate, coverages in zip(
+                self._coordinates, self._coverage_lists):
+            aligned_coverages = self._align_coverages(coverages, max_range)
+            row = [coordinate[key] for key in
+                   ["replicon", "start", "end", "strand"]] + aligned_coverages
+            row_df = self._init_coverage_dataframe(max_range, [row])
+            self._coverage_df = self._coverage_df.append(
+                row_df, ignore_index=True)
+
+    def _align_coverages(self, coverages, max_range):
+        filling = np.nan if self._skip_missing_coverages else 0.0
+        aligned_coverages = list(np.full(max_range, filling))
+        if self._matrix_alignment == "left":
+            aligned_coverages[:len(coverages)] = coverages
+        elif self._matrix_alignment == "center":
+            margin = round((max_range - len(coverages)) / 2)
+            aligned_coverages[margin:margin+len(coverages)] = coverages
+        elif self._matrix_alignment == "right":
+            left_margin = max_range - len(coverages)
+            aligned_coverages[
+                left_margin:left_margin+len(coverages)] = coverages
+        return aligned_coverages
+            
+    def _init_coverage_dataframe(self, max_range, data=None):
+        """
+        - max_range
+        - data - list of list containig data
+        """
+        return pd.DataFrame(
+            data, columns=["Replicon", "Start", "End", "Strand"] +
+            list(range(1, max_range + 1)))
+
+    def write_matrix_to_file(self):
+        self._coverage_df.to_csv(
+            "{}_matrix.csv".format(self._output_prefix),
+            sep="\t", index=False, na_rep="NaN")
 
     def calc_averages(self):
-        max_list_length = max(
-            [len(coverages) for coverages in self._all_coverage_lists])
-        matrix = []
-        for coverage_list in self._all_coverage_lists:
-            no_of_gaps_to_add_to_sides = round((
-                max_list_length - len(coverage_list))/2)
-            filler = np.nan if self._skip_missing_coverages else 0.0
-            coverage_list = [
-                filler] * no_of_gaps_to_add_to_sides + coverage_list + [
-                    filler] * no_of_gaps_to_add_to_sides
-            matrix.append(coverage_list)
-        matrix = np.array(matrix)
-        output_fh = open("{}_combined.csv".format(self._output_prefix), "w")
-        output_fh.write("\t".join([
-            "Pos", "Mean", "Median", "Standard deviation"]) + "\n")
-        for pos in range(max_list_length):
-            column = matrix[:, pos]
-            shifted_pos = pos - (max_list_length/2)
-            if self._ignore_zeros:
-                column = np.array(list(filter(lambda cov: cov != 0.0, column)))
-            if self._skip_missing_coverages:
-                column = np.array(
-                    list(filter(lambda cov: not math.isnan(cov), column)))
-            if np.sum(column) == 0.0:
-                continue
-            output_fh.write("\t".join([str(cell) for cell in [
-                shifted_pos,
-                np.mean(column),
-                np.median(column),
-                np.std(column)]]) + "\n")
-        output_fh.close()
+        average_df = pd.DataFrame(columns=["Value"] +
+                                  list(self._coverage_df.columns)[4:])
+        average_df["Value"] = [
+            "Median (all values)",
+            "Mean (all values)",
+            "Standard deviation (all values)",
+            "Median (zeros removed)",
+            "Mean (zeros removed)",
+            "Standard deviation (zeros removed)",
+            "Median (NAs removed)",
+            "Mean (NAs removed)",
+            "Standard deviation (NAs removed)",
+            "Median (zeroes and NAs removed)",
+            "Mean (zeroes and NAs removed)",
+            "Standard deviation (zeroes and NAs removed)"
+            ]
+        for column_name in list(self._coverage_df.columns)[4:]:
+            column = self._coverage_df[column_name]
+            column_without_zeros = np.array(list(
+                filter(lambda cov: cov != 0.0, column)))
+            column_without_na = np.array(list(
+                filter(lambda cov: not math.isnan(cov), column)))
+            column_without_zeros_and_na = np.array(list(filter(
+                lambda cov: not (math.isnan(cov) or cov == 0.0), column)))
+            average_df[column_name] = [
+                column.median(),
+                column.mean(),
+                column.std(),
+                np.median(column_without_zeros),
+                np.mean(column_without_zeros),
+                np.std(column_without_zeros),
+                np.median(column_without_na),
+                np.mean(column_without_na),
+                np.std(column_without_na),
+                np.median(column_without_zeros_and_na),
+                np.mean(column_without_zeros_and_na),
+                np.std(column_without_zeros_and_na)
+            ]
+        average_df.to_csv(
+            "{}_averages.csv".format(self._output_prefix),
+            sep="\t", index=False)
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,13 +1,64 @@
 import sys
 sys.path.append(".")
 import unittest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
 from covernantlib.extract import CoverageExtractor
 
 
 class TestCoverageExtractor(unittest.TestCase):
 
     def setUp(self):
-        self.coverage_extractor = CoverageExtractor()
+        self.coverage_extractor = CoverageExtractor(
+            coordinate_file="", coverage_file="", output_prefix="",
+            flip_reverse_strand=None, ignore_zeros=None,
+            skip_missing_coverages=None, matrix_alignment=None)
 
+    def test_init_coverage_dataframe_1(self):
+        df = self.coverage_extractor._init_coverage_dataframe(3)
+        self.assertListEqual(
+            list(df.columns),
+            ['Replicon', 'Start', 'End', 'Strand', 1, 2, 3])
+
+    def test_init_coverage_dataframe_2(self):
+        df = self.coverage_extractor._init_coverage_dataframe(
+            3, [["test_rep", 1, 4, "+", 4, 5, 6]])
+        self.assertListEqual(
+            list(df.columns),
+            ['Replicon', 'Start', 'End', 'Strand', 1, 2, 3])
+        
+    def test_generate_coverage_matrix(self):
+        self.coverage_extractor._coverage_lists = [[9, 9, 9, 9]]
+        self.coverage_extractor._coordinates = [
+            {"replicon": "test", "start": 1, "end": 5, "strand": "+"}]
+        self.coverage_extractor.generate_coverage_matrix()
+        # assert_frame_equal(
+        #     self.coverage_extractor._coverage_df,
+        #     pd.DataFrame(
+        #         [["test", 1, 5, "+", 9, 9, 9, 9]],
+        #         columns=['Replicon', 'Start', 'End', 'Strand',
+        #                  1, 2, 3, 4]))
+
+    def test_align_coverages_1(self):
+        """Alignment left"""
+        self.coverage_extractor._matrix_alignment = "left"
+        self.assertEqual(
+            self.coverage_extractor._align_coverages([1.0, 2.0, 3.0], 10),
+            [1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    def test_align_coverages_2(self):
+        """Alignment center"""
+        self.coverage_extractor._matrix_alignment = "center"
+        self.assertEqual(
+            self.coverage_extractor._align_coverages([1.0, 2.0, 3.0], 10),
+            [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 0.0, 0.0, 0.0])
+
+    def test_align_coverages_3(self):
+        """Alignment right"""
+        self.coverage_extractor._matrix_alignment = "right"
+        self.assertEqual(
+            self.coverage_extractor._align_coverages([1.0, 2.0, 3.0], 10),
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0])
+        
 if __name__ == "__main__":
         unittest.main()
