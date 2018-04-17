7074825f451d1094f201cc312d62f801ffe0a877
Konrad Förstner <konrad@foerstner.org>
2018-04-17 22:33:35 +0200

    Set version to 0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='COVERnant',
-    version='0.3.0',
+    version='0.3.1',
     packages=['covernantlib'],
     author='Konrad U. Förstner',
     author_email='konrad@foerstner.org',
